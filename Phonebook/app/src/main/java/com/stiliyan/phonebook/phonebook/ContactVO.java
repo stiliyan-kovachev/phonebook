729db53a9a6a9f1ee3b3fbb774fb5b6e45013c1d
Stiliyan <stiliqnk@gmail.com>
2017-10-16 11:09:24 +0300

    add empty activities, data models and data controller

---
+++ Phonebook/app/src/main/java/com/stiliyan/phonebook/phonebook/ContactVO.java
@@ -0,0 +1,13 @@
+package com.stiliyan.phonebook.phonebook;
+
+public class ContactVO {
+    public int id;
+    public  String name;
+    public String country;
+    public String code;
+    public String phone;
+    public String email;
+    public String gender;
+
+    public ContactVO() {}
+}
---
+++ Phonebook/app/src/main/java/com/stiliyan/phonebook/phonebook/DataController.java
@@ -0,0 +1,4 @@
+package com.stiliyan.phonebook.phonebook;
+
+public class DataController {
+}
